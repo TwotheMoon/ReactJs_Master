a2f920f31e9569225d6705e94b05d33c25b43dd1
2theMoon <dlaguddh1@gmail.com>
2021-12-07 17:49:44 +0900

    react 시계 만들기

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
 
 function App() {
+  const date = new Date();
+  const [dateTime, setDateTime] = useState({
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds()
+  });
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const date = new Date();
+      setDateTime({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <h1> get Start </h1>
-    </div>
+    <Father>
+      <div>{dateTime.hours}:{dateTime.minutes}:{dateTime.seconds}</div>
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+    </Father>
   );
 }
 
