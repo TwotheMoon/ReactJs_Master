5d6225cd3e2cf4c966f372d588312b2b69a3f06e
2theMoon <dlaguddh1@gmail.com>
2021-12-08 20:17:27 +0900

    TypeScript 이벤트 처리법
    - event 의 타입은 'any' 여서 오류가 났을때 확인하기 어려움
    - event에 타입을 정의해서 사용하자

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,12 +1,27 @@
+import React, { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 
 function App() {
 
+  const [value, setValue] = useState("");
+  // 이벤트에 대한 정의 : 이녀석은 HTML input 요소에 대한 이벤트이다
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },  // == setValue(event.currentTarget.value);
+    } = event;                   // 데이터 무결성을 위해 getter, setter
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
     <div>
-      <Circle borderColor="black" bgColor="teal" text="Yap!" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="username"></input>
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
