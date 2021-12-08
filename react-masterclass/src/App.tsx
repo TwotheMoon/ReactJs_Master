fec307429e70b56cfc78a6f32cb367f599ae528a
2theMoon <dlaguddh1@gmail.com>
2021-12-08 20:31:47 +0900

    종합 예제 - 다크모드 라이트 모드

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -3,25 +3,17 @@ import styled from "styled-components";
 
 function App() {
 
-  const [value, setValue] = useState("");
-  // 이벤트에 대한 정의 : 이녀석은 HTML input 요소에 대한 이벤트이다
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },  // == setValue(event.currentTarget.value);
-    } = event;                   // 데이터 무결성을 위해 getter, setter
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+  const H1 = styled.h1`
+  color:${(props) => props.theme.textColor};
+  `;
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={onChange} type="text" placeholder="username"></input>
-        <button>Log in</button>
-      </form>
+      <Container>
+        <H1>protected</H1>
+      </Container>
     </div>
   );
 }
---
+++ react-masterclass/src/styled.d.ts
@@ -0,0 +1,10 @@
+import "styled-components";
+
+// styled-components 테마 정의
+declare module "styled-components" {
+    export interface DefaultTheme {
+        textColor: string;
+        bgColor: string;
+        btnColor: string;
+    }
+}
