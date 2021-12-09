04a664af14519cd42f9a467576c8cdc6e22e4dae
2theMoon <dlaguddh1@gmail.com>
2021-12-09 18:08:46 +0900

    코인 디테일 페이지 데이터 타입 선언

--- react-masterclass/src/routes/Coin.tsx
+++ react-masterclass/src/routes/Coin.tsx
@@ -34,12 +34,69 @@ interface RouteState {
     name: string;
 }
 
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    contract: string;
+    platform: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface PriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: {
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        }
+    };
+}
+
 function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams<RouteParams>();
     const { state } = useLocation<RouteState>();   // Link to 로 보내어 온 객체 받기
-    const [info, setInfo] = useState({});
-    const [priceInfo, setPriceInfo] = useState({});
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
     useEffect(() => {
         (async () => {
             const infoData = await (
@@ -52,7 +109,7 @@ function Coin() {
             setPriceInfo(priceData);
             setLoading(false);
         })();
-    }, [])
+    }, [coinId])
     return (
         <Container>
             <Header>
@@ -61,7 +118,10 @@ function Coin() {
             {loading ? (
                 <Loader>Loaing...</Loader>
             ) : (
-                <span></span>
+                <div>
+                    <span>{info?.description}</span>
+                    <span>{priceInfo?.quotes.USD.ath_date}</span>
+                </div>
             )}
         </Container>
     );
