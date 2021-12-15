c46ab4dd194d383572ce4fc49a56f3268b448f35
2theMoon <dlaguddh1@gmail.com>
2021-12-15 20:48:16 +0900

    selector 사용법
    - 원하는 atom state를 가져와 변형후 값을 다시 리턴해주는 기능

--- react-masterclass/src/atoms.tsx
+++ react-masterclass/src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 // toDos[] 데이터 타입 정의
 export interface IToDo {
@@ -10,4 +10,16 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
+});
+
+export const toDoSelector = selector({
+    key: "toDoSelector",
+    get: ({ get }) => {
+        const toDos = get(toDoState);
+        return [
+            toDos.filter((toDo) => toDo.category === "TO_DO"),
+            toDos.filter((toDo) => toDo.category === "DOING"),
+            toDos.filter((toDo) => toDo.category === "DONE"),
+        ];   // 새 배열에 카테고리별로 나눠서 다시 생성
+    },
 });
