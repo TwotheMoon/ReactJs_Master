2f775882492df4910a2e4821e7ac15f809696881
2theMoon <dlaguddh1@gmail.com>
2021-12-15 12:35:09 +0900

    react-hook-form
    - useForm(); 선언
    - 인자로 제공해주는 함수 두 가지
    - register = onChange, onBlur, ref 상태 변화 데이터 제공
    - watch = 상태 변화 확인
    {...register("Key 값")} 으로 태그에 적용

--- react-masterclass/src/ToDoList.tsx
+++ react-masterclass/src/ToDoList.tsx
@@ -1,25 +1,54 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList(){
-    const [todo, setTodo] = useState("");
-    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-      const {
-          currentTarget: {value},
-      } = event;
-      setTodo(value);
-    };
-    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(todo);
-    };
-    
+// 라이브러리 없이 리액트로만 회원가입 폼
+// function ToDoList(){
+//     const [todo, setTodo] = useState("");
+//     const [toDoError, setToDoError] = useState("");
+//     const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+//       const {
+//           currentTarget: {value},
+//       } = event;
+//       setToDoError("");
+//       setTodo(value);
+//     };
+//     const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         if(todo.length < 10){
+//             return setToDoError("To do should be longer");
+//         }
+//         console.log("submit");
+//     };
+
+
+//     return (
+//     <div>
+//         <form onSubmit={onSubmit}>
+//             <input onChange={onChange} value={todo} placeholder="Write a to do"></input>
+//             <button>Add</button>
+//             {toDoError !== "" ? toDoError : null}
+//         </form>    
+//     </div>
+//     );
+// }
+
+// react-hook-form 사용 회원가입 폼
+function ToDoList() {
+    const { register, watch } = useForm();
+    // register : onChagne, onBlur, ref 관련 wathc : 폼에 모든 값을 주시
+    console.log(watch());
     return (
-    <div>
-        <form onSubmit={onSubmit}>
-            <input onChange={onChange} value={todo} placeholder="Write a to do"></input>
-            <button>Add</button>
-        </form>    
-    </div>
+        <div>
+            <form>          {/*es6 문법 toDo라는 이름의 함수로 반환하는 데이터 props 로 제공 */}
+                <input {...register("email")} placeholder="Email"></input>
+                <input {...register("firstName")} placeholder="First Name"></input>
+                <input {...register("lastName")} placeholder="Last Name"></input>
+                <input {...register("userName")} placeholder="Username"></input>
+                <input {...register("password")} placeholder="Password"></input>
+                <input {...register("passwordConfig")} placeholder="PasswordConfig"></input>
+                <button>Add</button>
+            </form>
+        </div>
     );
 }
 
