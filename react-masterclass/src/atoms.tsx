536671b05ccdce5732355c5ab9cfc66a95152e75
2theMoon <dlaguddh1@gmail.com>
2021-12-21 12:18:32 +0900

    윈도우 사이즈 확인

--- react-masterclass/src/atoms.tsx
+++ react-masterclass/src/atoms.tsx
@@ -1,12 +1,11 @@
 import { atom, selector } from "recoil";
 
-export enum Categories {
-    "TO_DO" = "TO_DO",
+export enum Categories { // enum 값들은 일반적으로 number 0 ,1 , 2
+    "TO_DO" = "TO_DO", // 그래서 실제 스트링값으로 다시선언
     "DOING" = "DOING",
     "DONE" = "DONE",
+    "DELETE" = "DELELTE",
 }
-
-type categories = "TO_DO" | "DOING" | "DONE";
 
 // toDos[] 데이터 타입 정의
 export interface IToDo {
@@ -16,7 +15,7 @@ export interface IToDo {
 }
 
 
-export const toDoState = atom<IToDo[]>({
+export const toDoState = atom<IToDo[]>({ // 새로 생성되는 toDo들이 해당 스테이트로 저장
     key: "toDo",
     default: [],
 });
@@ -27,7 +26,7 @@ export const categoryState = atom<Categories>({
     default: Categories.TO_DO,
 });
 
-export const toDoSelector = selector({
+export const toDoSelector = selector({  // todoState에 값들을 카테고리별로 변형
     key: "toDoSelector",
     get: ({ get }) => {
         const toDos = get(toDoState);
--- react-masterclass/src/components/ToDo.tsx
+++ react-masterclass/src/components/ToDo.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { Categories, IToDo, toDoState } from "../atoms";
 
 // ToDoList 에서 넘겨주는 모든 버튼의  text, category, id 인자로 받음
 function ToDo({ text, category, id }: IToDo) {
-    const setToDos = useSetRecoilState(toDoState);
+    const [toDos, setToDos] = useRecoilState(toDoState);
     const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const {
             currentTarget: { name },
@@ -17,10 +17,20 @@ function ToDo({ text, category, id }: IToDo) {
             const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
             const oldToDo = oldToDos[targetIndex]       // 바꿀 버튼 인덱스 기존배열에서[찾은 인덱스 번호]
             const newToDo = { text, id, category: name as any };  // 새로 세팅 name 을 category와 같은 문자열로 했기에 값을 name올 설정
-            console.log(oldToDo, newToDo);         // newToDo 타입이 string이여서 오류 발생 as any로 일시적 회피
+
             return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex + 1)];
         });
     };
+    const deleteTodo = () => {
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+            return [
+                ...oldToDos.slice(0, targetIndex),
+                ...oldToDos.slice(targetIndex + 1),
+            ];
+        });
+    }
+    localStorage.setItem("toDos", JSON.stringify(toDos));
 
     return (
         <li>
@@ -28,6 +38,7 @@ function ToDo({ text, category, id }: IToDo) {
             {category !== Categories.DOING && <button name={Categories.DOING} onClick={onClick}>Doing</button>}
             {category !== Categories.TO_DO && <button name={Categories.TO_DO} onClick={onClick}>To Do</button>}
             {category !== Categories.DONE && <button name={Categories.DONE} onClick={onClick}>Done</button>}
+            <button name={Categories.DELETE} onClick={deleteTodo}>Delete</button>
 
         </li >
     );
