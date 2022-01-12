36bc2a408f0afe792ab5a2058b8b956a5b10c08b
2theMoon <dlaguddh1@gmail.com>
2022-01-12 18:01:17 +0900

    Recap APP 생성

---
+++ recap/src/App.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+
+function App() {
+  return (
+    <>
+      <Father>
+      </Father>
+    </>
+  );
+}
+
+export default App;
