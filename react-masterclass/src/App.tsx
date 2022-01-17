033a0dc36ee5093df9edca562a2722903f6979e9
2theMoon <dlaguddh1@gmail.com>
2022-01-17 20:56:55 +0900

    recording

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,5 +1,7 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atoms";
 
 const Wrapper = styled.div`
   display: flex;
@@ -25,17 +27,28 @@ const Board = styled.div`
   min-height: 200px;
 `;
 
-const toDos = ["a", "b", "c", "d", "e", "f"];
 
 const Card = styled.div`
  border-radius: 5px;
  margin-bottom: 5px;
  padding: 5px 10px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
+ background-color: ${(props) => props.theme.cardColor};
+ `;
 
 function App() {
-  const onDragEnd = () => { };
+  const [toDos, setTodos] = useRecoilState(toDoState);
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => { // 드래그가 끝났을때 실행되는 함수
+    if (!destination) return; // 제자리에 드래그 했을 경우
+
+    setTodos((oldToDos) => {
+      const copyToDos = [...oldToDos];
+      // 1. source.index 에 해당하는 아이템 제거
+      copyToDos.splice(source.index, 1);
+      // 2. 지운 아이템 다시 destination.index로 돌려두기
+      copyToDos.splice(destination?.index, 0, draggableId);
+      return copyToDos;
+    })
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
@@ -44,9 +57,9 @@ function App() {
           <Droppable droppableId="one">
             {(magic) =>
               <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((todo, index) => <Draggable draggableId={todo} index={index}>
+                {toDos.map((toDo, index) => <Draggable key={toDo} draggableId={toDo} index={index}>
                   {(magic) => <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
-                    {todo}
+                    {toDo}
                   </Card>}
                 </Draggable>
                 )}
