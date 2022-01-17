85f8f9a8cfb7fb16ed4842943a127749f0dcd5c6
2theMoon <dlaguddh1@gmail.com>
2022-01-17 12:50:28 +0900

    스타일 먹이기 & placeholder

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,29 +1,60 @@
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
+
+const Card = styled.div`
+ border-radius: 5px;
+ margin-bottom: 5px;
+ padding: 5px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
 
 function App() {
   const onDragEnd = () => { };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
-        <Droppable droppableId="one">
-          {(magic) =>
-            <ul ref={magic.innerRef} {...magic.droppableProps}>
-              <Draggable draggableId="first" index={0}>
-                {(magic) => <li ref={magic.innerRef} {...magic.draggableProps}>
-                  <span {...magic.dragHandleProps}>🎈</span>
-                  One
-                </li>}
-              </Draggable>
-              <Draggable draggableId="second" index={1}>
-                {(magic) => <li ref={magic.innerRef} {...magic.draggableProps} >
-                  <span {...magic.dragHandleProps}>🎈</span>
-                  Two
-                </li>}
-              </Draggable>
-            </ul>}
-        </Droppable>
-      </div>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(magic) =>
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((todo, index) => <Draggable draggableId={todo} index={index}>
+                  {(magic) => <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+                    {todo}
+                  </Card>}
+                </Draggable>
+                )}
+                {magic.placeholder}
+              </Board>}
+          </Droppable>
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   );
 }
--- react-masterclass/src/styled.d.ts
+++ react-masterclass/src/styled.d.ts
@@ -3,8 +3,8 @@ import "styled-components";
 // styled-components 테마 정의
 declare module "styled-components" {
     export interface DefaultTheme {
-        textColor: string;
         bgColor: string;
-        accentColor: string;
+        boardColor: string;
+        cardColor: string;
     }
 }
