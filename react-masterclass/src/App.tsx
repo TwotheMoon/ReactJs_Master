798d8e4844300af9610bd829cb2c188a506da0a1
2theMoon <dlaguddh1@gmail.com>
2022-01-17 21:37:47 +0900

    보드 3개 나누기

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,12 +1,12 @@
-import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
-import DraggableCard from "./Components/DraggableCard";
+import Board from "./Components/Board";
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
+  max-width: 680px;
   width: 100%;
   height: 100vh;
   margin: 0 auto;
@@ -17,45 +17,32 @@ const Wrapper = styled.div`
 const Boards = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(1, 1fr);
+  gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
 `;
 
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
-`;
+
 
 function App() {
   const [toDos, setTodos] = useRecoilState(toDoState);
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => { // 드래그가 끝났을때 실행되는 함수
     if (!destination) return; // 제자리에 드래그 했을 경우
 
-    setTodos((oldToDos) => {
-      const copyToDos = [...oldToDos];
-      // 1. source.index 에 해당하는 아이템 제거
-      copyToDos.splice(source.index, 1);
-      // 2. 지운 아이템 다시 destination.index로 돌려두기
-      copyToDos.splice(destination?.index, 0, draggableId);
-      return copyToDos;
-    })
+    // setTodos((oldToDos) => {
+    //   const copyToDos = [...oldToDos];
+    //   // 1. source.index 에 해당하는 아이템 제거
+    //   copyToDos.splice(source.index, 1);
+    //   // 2. 지운 아이템 다시 destination.index로 돌려두기
+    //   copyToDos.splice(destination?.index, 0, draggableId);
+    //   return copyToDos;
+    // })
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) =>
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DraggableCard key={toDo} toDo={toDo} index={index} />
-                ))}
-                {magic.placeholder}
-              </Board>}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />)}
         </Boards>
       </Wrapper>
     </DragDropContext>
--- react-masterclass/src/atoms.tsx
+++ react-masterclass/src/atoms.tsx
@@ -1,7 +1,14 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 
+interface IToDoState {  // 이후에 추가될 보드때문에 toDoState에 한번더 타입 정의
+    [key: string]: string[];
+}
 
-export const toDoState = atom({
+export const toDoState = atom<IToDoState>({
     key: "toDo",
-    default: ["a", "b", "c", "d", "e", "f"],
+    default: {
+        to_do: ["a", "b"],
+        doing: ["c", "d", "e"],
+        done: ["f"],
+    },
 });
