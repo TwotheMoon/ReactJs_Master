86f2d06678f2f68a29bb8d59b3630bb26ff9233b
2theMoon <dlaguddh1@gmail.com>
2022-01-17 11:59:16 +0900

    selector

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,7 +1,22 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { hoursSelector, minutesState } from "./atoms";
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minutesState);
+  const [hours, setHours] = useRecoilState(hoursSelector);
+  const onMinutesChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+e.currentTarget.value);
+  };
+  const onHoursChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setHours(+e.currentTarget.value);
+  };
+
   return (
-    <div>확인</div>
+    <div>
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
+    </div>
   );
 }
 
