c3d4d621c4fa83be72121ff336ce0fc1daf420dd
2theMoon <dlaguddh1@gmail.com>
2022-01-20 16:53:02 +0900

    Header Part

---
+++ react-masterclass/src/Routes/Home.tsx
@@ -0,0 +1,8 @@
+function Home() {
+
+    return (
+        <div style={{ backgroundColor: "white", height: "200vh" }}>TV</div>
+    );
+}
+
+export default Home;
---
+++ react-masterclass/src/Routes/Tv.tsx
@@ -0,0 +1,8 @@
+function Tv() {
+
+    return (
+        <h1 style={{ marginTop: "100px" }}>TV</h1>
+    );
+}
+
+export default Tv;
